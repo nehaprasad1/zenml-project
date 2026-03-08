9efb559c074af747ed279859d7077a89cc7e9453
neha prasad <18.prasadneha@gmail.com>
2026-03-08 07:59:16 +0530

    Added data ingestion step

---
+++ steps/ingest_data.py
@@ -0,0 +1,37 @@
+import logging
+import pandas as pd
+from zenml import step
+"""
+ingesting data from the data path
+"""
+class IngestData:
+    """
+    Args:
+        data_path: path to the data
+    """
+    def __init__(self, data_path:str):
+        self.data_path = data_path
+        
+    def get_data(self):
+        logging.info(f"Ingessting data from {self.data_path}")
+        return pd.read_csv(self.data_path)
+    
+@step
+def ingest_data(data_path: str) -> pd.DataFrame:
+    
+    """
+    Ingesting data from the data path
+    Args:
+        data_path"path to the Data
+    Returns:
+        pd.DataFrame: the ingested data
+    """
+    
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e :
+        logging.error(f"Error while ingesting data: {e}")
+        raise e
+    
